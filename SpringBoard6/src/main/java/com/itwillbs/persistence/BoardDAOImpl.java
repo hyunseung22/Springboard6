21531904302b0c4e948b65a85f3f194048b71d80
ITWILL <gustmd6262@naver.com>
2024-10-16 17:12:50 +0900

    BoardDAO-listAll() 작업,boardMapper-listAll SQL구문작성

--- SpringBoard6/src/main/java/com/itwillbs/persistence/BoardDAOImpl.java
+++ SpringBoard6/src/main/java/com/itwillbs/persistence/BoardDAOImpl.java
@@ -1,28 +1,45 @@
 package com.itwillbs.persistence;
+
+import java.util.List;
+
 import javax.inject.Inject;
+
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
 import com.itwillbs.domain.BoardVO;
-import com.itwillbs.service.BoardServiceImpl;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO {
 	
-	@Repository
-	public class BoardDAOImpl implements BoardDAO {
-		// 디비연결정보를 처리하는 객체를 주입
-		@Inject
-		private SqlSession sqlSession;
-		
-		private static final Logger logger 
-			= LoggerFactory.getLogger(BoardDAOImpl.class);
-		private static final String NAMESPACE
-							="com.itwillbs.mapper.BoardMapper.";
+	// 디비연결정보를 처리하는 객체를 주입사용
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final Logger logger 
+	   = LoggerFactory.getLogger(BoardDAOImpl.class);
+	
+	private static final String NAMESPACE 
+	                   ="com.itwillbs.mapper.BoardMapper.";
+	
 	
 	@Override
 	public void createBoard(BoardVO vo) throws Exception {
-		logger.debug(" createBoard(BoardVO vo) 호출");
-		logger.debug(" 디비 연결 - SQL 호출 - 실행 " );
-		sqlSession.insert(NAMESPACE+ "insertBoard",vo);
+		logger.debug(" createBoard(BoardVO vo) 호출 ");
+		logger.debug(" 디비 연결 - SQL 호출 - 실행 ");
+		sqlSession.insert(NAMESPACE + "insertBoard",vo);
 	}
+
+
+	@Override
+	public List<BoardVO> listAll() throws Exception {
+		logger.debug(" listAll() 호출 ");
+		return sqlSession.selectList(NAMESPACE+ "listAll");
+	
+	}
+	
+	
 
 }
